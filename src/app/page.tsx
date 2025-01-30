1be2eabd970b95955dd38c1b21e6c5173d988302
IDRISSHACKER <idrisscoder@gmail.com>
2025-01-30 14:35:05 +0100

    Update version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
           className="min-h-screen bg-gradient-to-b from-green-100 to-blue-100 font-[family-name:var(--font-geist-sans)] flex flex-col items-center justify-center">
           <div className={"flex items-center justify-center flex-col gap-2 mb-16"}>
               <h1 className={"text-slate-900 font-black text-8xl text-center"}>DeepMemo</h1>
-              <p className={"text-gray-500"}>V1.0.0</p>
+              <p className={"text-gray-500"}>V1.0.1</p>
           </div>
           <InstallButton/>
       </div>
