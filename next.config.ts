00d2be2a0623f0ac220c3526b301d1fbd54ae8e6
IDRISSHACKER <idrisscoder@gmail.com>
2025-01-30 14:14:49 +0100

    Config pwa

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,13 @@
-import type {NextConfig} from "next";
+// next.config.js
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+});
 
-const withPWA = require('next-pwa')({
-    dest: 'public', // dossier où sera généré le sw
-    register: true, // auto-enregistrement du Service Worker
-    skipWaiting: true // mise à jour immédiate du SW
-})
-
-const nextConfig: NextConfig = withPWA({
+module.exports = withPWA({
     reactStrictMode: true,
-    experimental: {
-        turbo: {
-            resolveExtensions: [
-                '.mdx',
-                '.tsx',
-                '.ts',
-                '.jsx',
-                '.js',
-                '.mjs',
-                '.json',
-            ],
-        },
-    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
     },
-});
-
-export default nextConfig;
+})
