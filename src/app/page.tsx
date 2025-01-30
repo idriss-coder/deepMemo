a0a86831354b445ec951598c919cc64c32fb7192
IDRISSHACKER <idrisscoder@gmail.com>
2025-01-30 14:32:23 +0100

    Update version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import InstallButton from "@/app/install-btn";
 export default function Home() {
   return (
       <div
-          className="min-h-screen bg-gradient-to-b from-green-100 to-blue-100 font-[family-name:var(--font-geist-sans)] flex items-center justify-center">
+          className="min-h-screen bg-gradient-to-b from-green-100 to-blue-100 font-[family-name:var(--font-geist-sans)] flex flex-col items-center justify-center">
+          <div className={"flex items-center justify-center flex-col gap-2 mb-16"}>
+              <h1 className={"text-slate-900 font-black text-8xl text-center"}>DeepMemo</h1>
+              <p className={"text-gray-500"}>V1.0.0</p>
+          </div>
           <InstallButton/>
       </div>
   );
