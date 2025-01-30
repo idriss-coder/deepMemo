d8dda15bbe12a93b23ac15cd29f7bf77847e37ce
IDRISSHACKER <idrisscoder@gmail.com>
2025-01-30 12:59:07 +0100

    Update with my conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require('next-pwa')({
+    dest: 'public', // dossier où sera généré le sw
+    register: true, // auto-enregistrement du Service Worker
+    skipWaiting: true // mise à jour immédiate du SW
+})
+
+const nextConfig: NextConfig = withPWA({
+    reactStrictMode: true,
+    experimental: {
+        turbo: {
+            resolveExtensions: [
+                '.mdx',
+                '.tsx',
+                '.ts',
+                '.jsx',
+                '.js',
+                '.mjs',
+                '.json',
+            ],
+        },
+    },
+});
 
 export default nextConfig;
