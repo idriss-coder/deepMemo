cff75cebfe7121859e396bc146d7afd0a8e905e0
IDRISSHACKER <idrisscoder@gmail.com>
2025-02-06 17:09:20 +0100

    Update use search params

--- src/app/(main)/plaground/home/verses-list/new/infos/page.tsx
+++ src/app/(main)/plaground/home/verses-list/new/infos/page.tsx
@@ -3,7 +3,7 @@
 import {BackButton} from "@/components/customize/utils";
 import {AnimatePresence, motion} from "framer-motion"
 import {cn} from "@/lib/utils";
-import React, {useEffect, useMemo} from "react";
+import React, {Suspense, useEffect, useMemo} from "react";
 import {Textarea} from "@/components/ui/textarea";
 import {Button} from "@/components/ui/button";
 import {useRouter, useSearchParams} from "next/navigation";
@@ -124,94 +124,97 @@ export default function VersetInfos() {
     }
 
     return (
-        <div className="px-[20px] flex flex-col gap-[28px]">
-            <BackButton isClose={true} link="/plaground/home/verses-list" mainTitle={versetTitle}/>
-            <AnimatePresence mode="wait">
-                {curStep === 1 && (
-                    <motion.div key="step1" {...fadeInUp}>
-                        <ListGrid
-                            onSelect={(ns) => (ns.length ? setChapter(ns[0]) : setChapter(undefined))}
-                            listTitle="Chapitre"
-                            listCount={book.chapters}
-                            selectType="unique"
-                        />
-                    </motion.div>
-                )}
-                {curStep === 2 && (
-                    <motion.div key="step2" {...fadeInUp}>
-                        <ListGrid
-                            onSelect={(ns) => (ns.length ? setVersets(ns) : setVersets(undefined))}
-                            listTitle="Versets"
-                            listCount={book.chaptersVerses.find(cv => cv.chapter == chapter)?.verses || 0}
-                            selectType="range"
-                        />
-                        <motion.div
-                            className="h-[140px] p-[20px] flex flex-col gap-2 items-center justify-center bg-gradient-to-b from-[#141f25]/0 via-[#141f25] to-[#141f25] fixed bottom-0 right-0 w-full"
-                            initial={{opacity: 0, y: 50}}
-                            animate={{opacity: 1, y: 0}}
-                            transition={{delay: 0.2, duration: 0.3}}
-                        >
-                            {curStep == 2 && <Button
-                                variant={versets?.length ? "default" : "disabled"}
-                                disabled={!versets?.length}
-                                className="w-full"
-                                onClick={() => setCurStep(3)}
-                            >
-                                Continuer
-                            </Button>}
-                        </motion.div>
-                    </motion.div>
-                )}
-                {curStep === 3 && (
-                    <motion.div key="step3" {...fadeInUp} className="flex flex-col gap-6">
-                        <h2 className="text-2xl font-bold font-['Feather']">De quoi parle {versetTitle} ?</h2>
-                        <div className="flex flex-col gap-4">
-                            <Textarea
-                                rows={3}
-                                value={description}
-                                onChange={e => setDescription(e.currentTarget.value)}
-                                autoFocus={true}
+        <Suspense fallback={<div>Laoding...</div>}>
+            <div className="px-[20px] flex flex-col gap-[28px]">
+                <BackButton isClose={true} link="/plaground/home/verses-list" mainTitle={versetTitle}/>
+                <AnimatePresence mode="wait">
+                    {curStep === 1 && (
+                        <motion.div key="step1" {...fadeInUp}>
+                            <ListGrid
+                                onSelect={(ns) => (ns.length ? setChapter(ns[0]) : setChapter(undefined))}
+                                listTitle="Chapitre"
+                                listCount={book.chapters}
+                                selectType="unique"
                             />
-                            <div className="text-[#4e5b64] text-sm font-normal leading-snug">
-                                Vous n&apos;etes pas oubligé de mettre le contenu du verset dans son entiereté, juste un
-                                aperçu pour
-                                vous rappeler rapidment
+                        </motion.div>
+                    )}
+                    {curStep === 2 && (
+                        <motion.div key="step2" {...fadeInUp}>
+                            <ListGrid
+                                onSelect={(ns) => (ns.length ? setVersets(ns) : setVersets(undefined))}
+                                listTitle="Versets"
+                                listCount={book.chaptersVerses.find(cv => cv.chapter == chapter)?.verses || 0}
+                                selectType="range"
+                            />
+                            <motion.div
+                                className="h-[140px] p-[20px] flex flex-col gap-2 items-center justify-center bg-gradient-to-b from-[#141f25]/0 via-[#141f25] to-[#141f25] fixed bottom-0 right-0 w-full"
+                                initial={{opacity: 0, y: 50}}
+                                animate={{opacity: 1, y: 0}}
+                                transition={{delay: 0.2, duration: 0.3}}
+                            >
+                                {curStep == 2 && <Button
+                                    variant={versets?.length ? "default" : "disabled"}
+                                    disabled={!versets?.length}
+                                    className="w-full"
+                                    onClick={() => setCurStep(3)}
+                                >
+                                    Continuer
+                                </Button>}
+                            </motion.div>
+                        </motion.div>
+                    )}
+                    {curStep === 3 && (
+                        <motion.div key="step3" {...fadeInUp} className="flex flex-col gap-6">
+                            <h2 className="text-2xl font-bold font-['Feather']">De quoi parle {versetTitle} ?</h2>
+                            <div className="flex flex-col gap-4">
+                                <Textarea
+                                    rows={3}
+                                    value={description}
+                                    onChange={e => setDescription(e.currentTarget.value)}
+                                    autoFocus={true}
+                                />
+                                <div className="text-[#4e5b64] text-sm font-normal leading-snug">
+                                    Vous n&apos;etes pas oubligé de mettre le contenu du verset dans son entiereté,
+                                    juste un
+                                    aperçu pour
+                                    vous rappeler rapidment
+                                </div>
                             </div>
-                        </div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
 
-            {curStep === 3 && <motion.div
-                className="h-[140px] p-[20px] flex flex-col gap-2 items-center justify-center bg-gradient-to-b from-[#141f25]/0 via-[#141f25] to-[#141f25] fixed bottom-0 right-0 w-full"
-                initial={{opacity: 0, y: 0}}
-                animate={{opacity: 1, y: 0}}
-                transition={{delay: 0.2, duration: 0.3}}
-            >
-                <div className={"w-full flex flex-col gap-2"}>
-                    <Button
-                        variant={(isUpdate ? (description == mockedDescription) || !description : (!description)) ? "disabled" : "default"}
-                        className="w-full"
-                        disabled={isUpdate ? (description == mockedDescription) || !description : (!description)}
-                        onClick={isUpdate ? handleUpdateVerset : handleAddVerset}
-                    >
-                        {isUpdate ? "Mettre à jour le verset" : "Ajouter le verset"}
-                    </Button>
-                    {isUpdate && <Button
-                        variant={"textRed"}
-                        onClick={() => setRequestDelete(true)}
-                    >
-                        Supprimer le verset
-                    </Button>}
-                </div>
-            </motion.div>}
+                {curStep === 3 && <motion.div
+                    className="h-[140px] p-[20px] flex flex-col gap-2 items-center justify-center bg-gradient-to-b from-[#141f25]/0 via-[#141f25] to-[#141f25] fixed bottom-0 right-0 w-full"
+                    initial={{opacity: 0, y: 0}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{delay: 0.2, duration: 0.3}}
+                >
+                    <div className={"w-full flex flex-col gap-2"}>
+                        <Button
+                            variant={(isUpdate ? (description == mockedDescription) || !description : (!description)) ? "disabled" : "default"}
+                            className="w-full"
+                            disabled={isUpdate ? (description == mockedDescription) || !description : (!description)}
+                            onClick={isUpdate ? handleUpdateVerset : handleAddVerset}
+                        >
+                            {isUpdate ? "Mettre à jour le verset" : "Ajouter le verset"}
+                        </Button>
+                        {isUpdate && <Button
+                            variant={"textRed"}
+                            onClick={() => setRequestDelete(true)}
+                        >
+                            Supprimer le verset
+                        </Button>}
+                    </div>
+                </motion.div>}
 
-            <DeleteVersetConfirmScreen
-                state={requestDelete ? "opened" : "closed"}
-                onCloseCancel={() => setRequestDelete(false)}
-                onDeleteConfirm={handleDeleteVerset}
-            />
-        </div>
+                <DeleteVersetConfirmScreen
+                    state={requestDelete ? "opened" : "closed"}
+                    onCloseCancel={() => setRequestDelete(false)}
+                    onDeleteConfirm={handleDeleteVerset}
+                />
+            </div>
+        </Suspense>
     )
 }
 
