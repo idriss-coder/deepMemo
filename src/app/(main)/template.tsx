ca075932d7cf526022f70d645c1b80bff757369c
IDRISSHACKER <idrisscoder@gmail.com>
2025-02-08 15:56:49 +0100

    Update game sound

---
+++ src/app/(main)/template.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react";
+
+
+const Template: React.FC<{ children: React.ReactNode }> = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+
+export default Template
