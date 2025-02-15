2195d3c564eeafeeeec672c8598af500a007cf30
IDRISSHACKER <idrisscoder@gmail.com>
2025-02-15 17:23:49 +0100

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 // next.config.js
 const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
-    disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
