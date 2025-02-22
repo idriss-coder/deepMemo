e2c0c10486a7d1d179da21f8931011dba7e0e07b
IDRISSHACKER <idrisscoder@gmail.com>
2025-02-22 17:52:40 +0100

    Update privacy

--- src/app/plaground/use-term/page.tsx
+++ src/app/plaground/use-term/page.tsx
@@ -36,22 +36,26 @@ export default function UseTermPage() {
                         <div className="w-[60px] h-[5px] relative bg-[#38454e] rounded-[20px]"/>
                     </div>
                     <div className={"pt-[20px] pb-[120px] text-white/80 text-base font-medium leading-relaxed"}>
-                        1. Introduction
-                        Bienvenue sur KuizLang (ci-après « l’Application »), une plateforme de quiz linguistiques et
-                        éducatifs similaire à Duolingo. Les présentes Conditions Générales d’Utilisation (ci-après « CGU
+                        <h4 className={"text-lg font-bold font-['geist']"}> 1. Introduction</h4>
+                        Bienvenue sur deepMemo (ci-après « l’Application »), une plateforme de quiz linguistiques et
+                        éducatifs similaire. Les présentes Conditions Générales d’Utilisation (ci-après « CGU
                         »)
                         régissent votre accès à l’Application et son utilisation. Veuillez lire attentivement ces CGU
                         avant
                         de vous inscrire ou d’utiliser les services proposés. En accédant à l’Application, vous acceptez
                         d’être lié par ces CGU.
 
-                        2. Objet
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}>2. Objet</h4>
+
                         Les présentes CGU ont pour objet de définir les modalités d’utilisation de l’Application.
-                        KuizLang
+                        deepMemo
                         fournit des quiz, des leçons et divers contenus pédagogiques visant à faciliter l’apprentissage
                         des
                         langues. Ces services peuvent être amenés à évoluer.
-                        3. Inscription et Compte Utilisateur
+
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}>3. Inscription et Compte
+                            Utilisateur</h4>
+
                         Création de compte : Pour accéder à toutes les fonctionnalités de l’Application, vous devrez
                         créer
                         un compte en fournissant une adresse e-mail valide et en choisissant un mot de passe. Vous vous
@@ -62,8 +66,10 @@ export default function UseTermPage() {
                         Âge minimum : L’utilisation de l’Application est réservée aux personnes âgées d’au moins 13 ans
                         ou
                         ayant l’autorisation parentale (selon la législation en vigueur dans votre pays).
-                        4. Utilisation de l’Application
-                        Licence d’utilisation : Sous réserve du respect des présentes CGU, KuizLang vous accorde une
+
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}>4. Utilisation de l’Application</h4>
+
+                        Licence d’utilisation : Sous réserve du respect des présentes CGU, deepMemo vous accorde une
                         licence
                         personnelle, non exclusive, non transférable et révocable pour accéder à l’Application et
                         l’utiliser
@@ -76,24 +82,31 @@ export default function UseTermPage() {
                         piratage ou toute autre action malveillante.
                         Utiliser l’Application à des fins illégales, diffamatoires, frauduleuses ou portant atteinte aux
                         droits de tiers.
-                        Suspension de l’accès : KuizLang se réserve le droit de suspendre ou de résilier l’accès à
+                        Suspension de l’accès : deepMemo se réserve le droit de suspendre ou de résilier l’accès à
                         l’Application, sans préavis, en cas de violation des présentes CGU ou de comportement
                         inapproprié.
-                        5. Contenu et Propriété Intellectuelle
-                        Contenu KuizLang : Tous les éléments présents sur l’Application (textes, graphiques, logos,
+
+
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}> 5. Contenu et Propriété
+                            Intellectuelle</h4>
+
+                        Contenu deepMemo : Tous les éléments présents sur l’Application (textes, graphiques, logos,
                         sons,
-                        vidéos, quiz, etc.) sont la propriété exclusive de KuizLang ou de ses concédants et sont
+                        vidéos, quiz, etc.) sont la propriété exclusive de deepMemo ou de ses concédants et sont
                         protégés
                         par la législation applicable en matière de droits d’auteur et de propriété intellectuelle.
                         Contenu Utilisateur : Les utilisateurs peuvent être amenés à soumettre du contenu (réponses,
                         commentaires, messages, etc.). Vous restez propriétaire de ce que vous publiez, mais vous
                         accordez à
-                        KuizLang une licence mondiale, gratuite et non exclusive pour l’affichage et la diffusion du
+                        deepMemo une licence mondiale, gratuite et non exclusive pour l’affichage et la diffusion du
                         contenu
                         dans le cadre des fonctionnalités de l’Application.
                         Signalement d’un contenu illicite : Pour signaler un contenu litigieux, veuillez contacter notre
                         service client à l’adresse indiquée dans l’Application.
-                        6. Modalités Financières
+
+
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}> 6. Modalités Financières</h4>
+
                         Application gratuite et offres payantes : L’Application propose une version gratuite comprenant
                         des
                         quiz et des fonctionnalités de base. Des abonnements payants ou des achats in-app (par exemple,
@@ -105,23 +118,29 @@ export default function UseTermPage() {
                         Remboursements : Les politiques de remboursement dépendent des conditions imposées par la
                         plateforme
                         de distribution (App Store, Google Play) ou, le cas échéant, par la législation applicable.
-                        7. Protection de la Vie Privée
+
+
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}> 7. Protection de la Vie Privée</h4>
+
                         Votre utilisation de l’Application est soumise à notre [Politique de Confidentialité], qui
                         décrit la
                         manière dont nous collectons, utilisons et protégeons vos données personnelles. En utilisant
                         l’Application, vous acceptez la collecte et l’utilisation de vos informations conformément à
                         cette
                         Politique.
-                        8. Limitation de Responsabilité
-                        Disponibilité de l’Application : KuizLang met en œuvre tous les moyens raisonnables pour assurer
+
+
+                        <h4 className={"text-lg font-bold font-['geist'] mt-5"}>8. Limitation de Responsabilité</h4>
+
+                        Disponibilité de l’Application : deepMemo met en œuvre tous les moyens raisonnables pour assurer
                         la
                         disponibilité de l’Application, mais ne garantit pas l’absence d’interruptions ou d’erreurs.
                         Exclusion de garantie : L’Application est fournie « en l’état ». Dans la mesure permise par la
                         loi,
-                        KuizLang exclut toute garantie explicite ou implicite, y compris les garanties de qualité
+                        deepMemo exclut toute garantie explicite ou implicite, y compris les garanties de qualité
                         marchande,
                         d’adaptation à un usage particulier ou de non-contrefaçon.
-                        Responsabilité limitée : Dans la mesure permise par la loi, KuizLang ne pourra être tenu
+                        Responsabilité limitée : Dans la mesure permise par la loi, deepMemo ne pourra être tenu
                         responsable
                         des dommages directs ou indirects résultant de l’utilisation ou de l’impossibilité d’utiliser
                         l’Application.
